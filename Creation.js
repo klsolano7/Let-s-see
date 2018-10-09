4d1984cc046d616873bef28480869e293e33a2b3
Kenneth Solano <klsolano77@gmail.com>
2018-10-09 15:44:49 -0800

    Time to create

--- Creation.js
+++ Creation.js
@@ -1,4 +1,3 @@
-//Java scripts//
-
-
-//Time to get back to work //
+console.log('Lets get this party started')
+console.log(5 * 7)
+console.log('What' + ' ' + 'does ' + ' ' + 'success' + 'look' + 'like')
